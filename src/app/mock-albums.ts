565a61c5dd5fdb6bff49686e1e05f54dd05cf2c1
tyler kostelak <tkostelak@gmail.com>
2018-04-03 16:46:36 -0700

    finish first day of album lesson, adds lologic to display ablum list, details

---
+++ src/app/mock-albums.ts
@@ -0,0 +1,20 @@
+// import { Injectable } from '@angular/core';
+// import { Album } from './album.model';
+// import { ALBUMS } from './mock-albums';
+// import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+//
+//
+// @Injectable()
+// export class AlbumService {
+//   albums: FirebaseListObservable<any[]>;
+//
+//   constructor(private database: AngularFireDatabase) {
+//   this.albums = database.list('albums');
+// }
+//
+// getAlbums(){
+//     return this.albums;
+//   }
+//
+//
+// }
